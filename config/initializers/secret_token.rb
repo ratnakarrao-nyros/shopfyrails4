b3dc88e6d0c55e329bd964be51cb372d06e6ecd0
ratnakarrao-nyros <ratnakarrao_nyros@yahoo.com>
2013-11-07 12:05:35 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopfyrails4::Application.config.secret_key_base = '7d4c1bfc8a043ed6a726ec1e4bd6997bf680f2fd98196301dfafadf55ffcba7c6dc2176e464a69432904cda8f6a2f8b5c19aea3f7019832ff51c464d3e05751f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shopfyrails4::Application.config.session_store :cookie_store, key: '_shopfyrails4_session'
